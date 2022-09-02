02454334ab6a4c42836c9354dc92f5997a39f130
Delfi sobrado <96445707+delfisobrado@users.noreply.github.com>
2022-09-02 19:36:55 -0300

    try

---
+++ index.js
@@ -0,0 +1,10 @@
+const singInBtn = document.getElementById("signInBtn");
+const nombre = document.getElementById('nombre');
+const signUpBtn = document.getElementById('signUpBtn');
+
+
+signInBtn.addEventListener('click', function() {
+
+const nombreUsuario = JSON.parse( localStorage.getItem('nombre'))
+   
+})
